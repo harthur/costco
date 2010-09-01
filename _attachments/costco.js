796d37edf5573eb9a9b0b70d416bed25c5ea800e
harthur <fayearthur@gmail.com>
2010-09-01 12:38:11 -0700

    multiple dbs, back to 'return doc;' syntax

--- _attachments/costco.js
+++ _attachments/costco.js
@@ -1,39 +1,73 @@
 $(document).ready(function() {
-  $("#map-function").val("function(doc) {\n\n}");
-  $('#update-button').click(costco.mapDocs);
-  $('#update-container').hide();
+  $("#map-function").val("function(doc) {\n  \n  return doc;\n}");
+  $("#map-function").focus().get(0).setSelectionRange(18, 18); 
+  
+  $("#update-button").click(costco.mapDocs);
+  $("#update-container").hide();
+  $("#status").click(function() {$("#status").empty()});
   
   $("#continue-button").click(function() {
     $("#status").text("updating docs...");
     costco.updateDocs(function() {
-      $("#status").text('Docs successfully updated');
+      $("#status").html("<span class='success'>Docs successfully updated</span>");
     });
-    $('#update-container').hide();
+    $("#update-container").hide();
   });
 
   $("#cancel-button").click(function() {
     $('#update-container').hide();
+    $('#status').empty();
+  });
+
+  $.couch.allDbs({
+    success: function(dbs) {
+      dbs.forEach(function(db){
+        $("<option></option>").val(db).html(db).appendTo("#db-select");
+      });
+      $.couch.app(function(app) {
+        $('#db-select').val(app.db.name);
+      });
+    },
+    error: function(req, status, err) {
+      alert("error getting all dbs " + err)
+    }
   });
 });
 
 var costco = {
   toUpdate : [],
+  
+  getDb : function() {
+    return $.couch.db($("#db-select").val());
+  },
 
   mapDocs : function() {
+    $("#status").html("<span>Computing changes...</span>");
+
     var funcString = $("#map-function").val();
-    eval("var editFunc = " + funcString);
-    
+    try {
+      eval("var editFunc = " + funcString);
+    } catch(e) {
+      $("#status").html("<span class='error'>error evaluating function: "
+        + e + "</span>");
+      return;
+    }
+
     costco.toUpdate = [];
     var deleted = 0
-      , edited = 0;
+      , edited = 0
+      , failed = 0;
 
     costco.getDocs(function(data) {
       var rows = data.rows;
       rows.forEach(function(row) {
         var doc = row.doc;
-        var updated = _.clone(doc);
-        editFunc(updated); // mutate doc
-
+        try {
+          var updated = editFunc(_.clone(doc));
+        } catch(e) {
+          failed++; // ignore if it throws on this doc
+          return;
+        }
         if(!updated) {
           doc._deleted = true;
           costco.toUpdate.push(doc);
@@ -44,9 +78,12 @@ var costco = {
           edited++;
         }
       });
-
-      $("#status").text("about to edit " + edited
-              + " docs and delete " + deleted + " docs");
+      // todo: make template for this
+      $("#status").html("<span class='warning'>About to edit " + edited
+              + " docs and delete " + deleted + " docs from "
+              + costco.getDb().name  + "</span>");
+      if(failed)
+        $("#status").append(". Edit function threw on " + failed + " docs");
       $("#update-container").show();
     });
   },
@@ -55,25 +92,23 @@ var costco = {
     if(!costco.toUpdate.length)
       return callback();
 
-    $.couch.app(function(app) {
-      app.db.bulkSave({docs: costco.toUpdate}, {
-        success: callback,
-        error: function(req, status, err) {
-          alert('error bulk saving docs: ' + err);
-        }
-      });
+    costco.getDb().bulkSave({docs: costco.toUpdate}, {
+      success: callback,
+      error: function(req, status, err) {
+        $("#status").html("<span class='error'>error updating docs: "
+           + err + "</span>");
+      }
     });
   },
 
   getDocs : function(callback) {
-    $.couch.app(function(app) {
-      app.db.allDocs({
-        include_docs : true,
-        success : callback,
-        error: function(req, status, err) {
-          alert('error retrieving docs: ' + err);
-        }
-      });
+    costco.getDb().allDocs({
+      include_docs : true,
+      success : callback,
+      error: function(req, status, err) {
+        $("#status").html("<span class='error'>error retrieving docs: "
+           + err + "</span>");
+      }
     });
   }
 }
